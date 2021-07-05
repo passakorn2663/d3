44912be25598a599d50a4bad39eaa4009a507ce6
passakorn2663 <passakornbuakeaw@gmail.com>
2021-07-05 21:06:58 +0700

    almost finish

--- bin/d3.dart
+++ bin/d3.dart
@@ -26,6 +26,11 @@ class Clock {
   void changeState(int index) {
     currentState = index;
   }
+
+  @override
+  String toString() {
+    return ('${hour.toString()} : ${minute.toString()}');
+  }
 }
 
 abstract class ClockState {
@@ -39,8 +44,8 @@ abstract class ClockState {
 class IdleState extends ClockState {
   @override
   void turnOn(Clock clock) {
-    if (clock.hour != 0 && clock.minute != 0) {
-      print(clock.hour + clock.minute);
+    if (clock.hour != 0 || clock.minute != 0) {
+      print(clock);
     } else
       // ignore: curly_braces_in_flow_control_structures
       set(clock);
@@ -56,6 +61,7 @@ class SettingHoursState extends ClockState {
   @override
   void inc(Clock clock) {
     clock.hour = (clock.hour + 1) % 24;
+    print(clock);
   }
 
   @override
@@ -68,6 +74,7 @@ class SettingMinsState extends ClockState {
   @override
   void inc(Clock clock) {
     clock.minute = (clock.minute + 1) % 60;
+    print(clock);
   }
 
   @override
@@ -76,9 +83,41 @@ class SettingMinsState extends ClockState {
   }
 }
 
+String input = '''
+on 18 0
+set 
+inc 
+set 
+inc 
+set 
+inc 
+inc 
+set 
+''';
+
 void main(List<String> arguments) {
-  var time = (stdin.readLineSync() ?? '').split(' ');
-  print(time);
-  int a = 18;
-  print(a % 18);
+  // var time = (stdin.readLineSync() ?? '').split(' ');
+  // print(time);
+
+  // Clock clock;
+
+  var line = input.split('\n');
+  var first_line = line[0].split(' ');
+  var clock;
+
+  if (first_line[1] == 0 && first_line[2] == 0) {
+    clock = Clock(0, 0);
+  } else {
+    clock = Clock(int.parse(first_line[1]), int.parse(first_line[2]));
+  }
+
+  clock.turnOn();
+
+  for (var i = 0; i < line.length; i++) {
+    if (line[i] == 'set') {
+      clock.set();
+    } else if (line[i] == 'inc') {
+      clock.inc();
+    }
+  }
 }
