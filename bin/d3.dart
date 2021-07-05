fa88bb8fe1e2d8b8f6c5338d8143147918ff7c5e
passakorn2663 <passakornbuakeaw@gmail.com>
2021-07-05 19:10:44 +0700

    15

--- bin/d3.dart
+++ bin/d3.dart
@@ -1,3 +1,13 @@
+class Clock {
+  final int hour;
+  final int minute;
+  Clock(this.hour, this.minute);
+}
+
 void main(List<String> arguments) {
   print('Hello world!');
 }
+
+void set() {}
+
+void inc() {}
