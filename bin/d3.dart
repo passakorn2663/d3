d53ff46317edcbef71ffa3f18e12390c92754b0e
passakorn2663 <passakornbuakeaw@gmail.com>
2021-07-05 20:14:43 +0700

    adjust state

--- bin/d3.dart
+++ bin/d3.dart
@@ -1,23 +1,34 @@
 import 'dart:io';
 
 class Clock {
+  List<ClockState> state = [
+    IdleState(),
+    SettingHoursState(),
+    SettingMinsState()
+  ];
+  int currentState = 0;
   int hour;
   int minute;
   Clock(this.hour, this.minute);
 
-  // void hourIncrement() {
-  //   hour = (hour + 1) % 24;
-  // }
+  void turnOn() {
+    state[currentState].turnOn(this);
+  }
 
-  // void minIncrement() {
-  //   minute = (minute + 1) % 60;
-  // }
+  void set() {
+    state[currentState].set(this);
+  }
+
+  void inc() {
+    state[currentState].inc(this);
+  }
+
+  void changeState(int index) {
+    currentState = index;
+  }
 }
 
 abstract class ClockState {
-  final int hour;
-  final int minute;
-  ClockState(this.hour, this.minute);
   void turnOn(Clock clock) {}
 
   void set(Clock clock) {}
@@ -26,32 +37,48 @@ abstract class ClockState {
 }
 
 class IdleState extends ClockState {
-  IdleState(int hour, int minute) : super(hour, minute);
+  @override
+  void turnOn(Clock clock) {
+    if (clock.hour != 0 && clock.minute != 0) {
+      print(clock.hour + clock.minute);
+    } else
+      // ignore: curly_braces_in_flow_control_structures
+      set(clock);
+  }
+
+  @override
+  void set(Clock clock) {
+    clock.changeState(1);
+  }
 }
 
 class SettingHoursState extends ClockState {
-  SettingHoursState(int hour, int minute) : super(hour, minute);
-
   @override
   void inc(Clock clock) {
     clock.hour = (clock.hour + 1) % 24;
   }
+
+  @override
+  void set(Clock clock) {
+    clock.changeState(2);
+  }
 }
 
 class SettingMinsState extends ClockState {
-  SettingMinsState(int hour, int minute) : super(hour, minute);
-
   @override
   void inc(Clock clock) {
     clock.minute = (clock.minute + 1) % 60;
+  }
+
+  @override
+  void set(Clock clock) {
+    clock.changeState(0);
   }
 }
 
 void main(List<String> arguments) {
   var time = (stdin.readLineSync() ?? '').split(' ');
   print(time);
+  int a = 18;
+  print(a % 18);
 }
-
-void set(int curNum) {}
-
-void inc(int hour, int minute) {}
